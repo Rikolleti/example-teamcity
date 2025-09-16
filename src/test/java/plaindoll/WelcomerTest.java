1a810fa66a760ce485fae475cd1ca755c4b91937
Azhdar Mamedov <rikolleti@gmail.com>
2025-09-16 09:16:16 +0000

    fix: duplicate test method welcomerSaysHunter

--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -19,8 +19,7 @@ public class WelcomerTest {
 	}
 	@Test
 	public void welcomerSaysHunter() {
-		assertThat(welcomer.sayWelcome(), containsString("hunter"));
-		assertThat(welcomer.sayFarewell(), containsString("hunter"));
+		assertThat(welcomer.sayHunter(), containsString("hunter"));
 	}
 	@Test
 	public void welcomerSaysSilver(){
@@ -29,10 +28,5 @@ public class WelcomerTest {
 	@Test
 	public void welcomerSaysSomething(){
 		assertThat(welcomer.saySome(), containsString("something"));
-	}
-	@Test
-	public void welcomerSaysHunter() {
-	    String reply = welcomer.sayHunter();
-	    assertTrue(reply.contains("hunter"));
 	}
 }
